a0894c6936b7d813a875a53a096191caea096d3d
Jeferson Farias Calazans <calazans10@gmail.com>
2014-03-23 10:47:59 -0300

    Add auth and CRUD to app

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
-var load = require('express-load');
-var engine = require('ejs-locals');
 var app = express();
+var load = require('express-load');
+var error = require('./middleware/error');
+var engine = require('ejs-locals');
 
 app.engine('ejs', engine);
 app.set('views', __dirname + '/views');
@@ -10,7 +11,11 @@ app.use(express.cookieParser('ntalk'));
 app.use(express.session());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(__dirname + '/public'));
+app.use(error.notFound);
+app.use(error.serverError);
 
 load('models')
   .then('controllers')
